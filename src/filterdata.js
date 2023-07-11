102dd0cc181c10cb8c6433c3f79fa9ca9e49eeb7
Julia Gangi <julia.gangi@publicissapient.com>
2023-07-11 10:04:24 -0400

    commit

--- src/filterdata.js
+++ src/filterdata.js
@@ -22,3 +22,26 @@ print(dict)
 json_data = json.dumps(dict)
 print(json_data)
 
+/*
+
+let dict = {};
+for (let model in trims) {
+    dict[model] = {};
+    let trim_data = trims[model];
+    for (let trim of trim_data) {
+        let arr = [];
+        for (let loc in data) {
+            if (Math.random() > .5) {
+                arr.push(data[loc]["name"]);
+            }
+        }
+        dict[model][trim] = arr;
+    }
+}
+
+//console.log(dict);
+
+let json_data = JSON.stringify(dict);
+console.log(json_data);
+
+*/
