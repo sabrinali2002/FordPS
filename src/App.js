84ed7a5bbc1dc02f1e40143f24e4ad7320bd0200
Sabrina Li <sabli1@publicisgroupe.net>
2023-07-17 13:51:22 -0400

    small fix

--- src/App.js
+++ src/App.js
@@ -124,7 +124,6 @@ function App() {
         }}>Info about a specific car</button>
       <button className = "menu" onClick={() => {
         handleUserInput('A');
-        setMenuButtons([]);
         }}>Car recommendation</button>
       <button className = "menu" onClick={() => {
         handleUserInput('D');
